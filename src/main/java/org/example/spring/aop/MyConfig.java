1761991c56740626d9ee2bbc8c299a50ce61a208
lanwar-prog <lanwar@mail.ru>
2021-09-26 00:25:00 +0400

    25 Advice type @Befor

--- src/main/java/org/example/spring/aop/MyConfig.java
+++ src/main/java/org/example/spring/aop/MyConfig.java
@@ -2,8 +2,10 @@ package org.example.spring.aop;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan("org.example.spring.aop")
+@EnableAspectJAutoProxy
 public class MyConfig {
 }
